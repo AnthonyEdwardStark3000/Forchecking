2e1b4e8048744e653107c946d65f99b0f00995d9
AnthonyEdwardStark3000 <catchmesureshbabu8@gmail.com>
2022-08-04 22:25:44 +0530

    Node js Server

--- Node JS/NodeServer-1/app.js
+++ Node JS/NodeServer-1/app.js
@@ -1,12 +1,6 @@
 const http = require('http');
-const server = http.createServer(function(req, res){
-    console.log(req);
-    //process.exit();
-    res.write('<html>');
-    res.write('<body>');
-    res.write('<h1>Printing from node js</h1>');
-    res.write('</body>');
-    res.write('</html>');
-});
+const routes = require('./routes');
+
+const server = http.createServer(routes);
 
 server.listen(3000);
