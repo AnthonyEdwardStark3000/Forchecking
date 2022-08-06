0e121326710c01fe7f745a4421d1a921c7931e50
AnthonyEdwardStark3000 <catchmesureshbabu8@gmail.com>
2022-08-06 22:02:21 +0530

    Express JS

--- Node JS/ExpressJS-2/app.js
+++ Node JS/ExpressJS-2/app.js
@@ -1,19 +1,33 @@
-const http = require('http');
+// const http = require('http');
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.use((req, res, next)=>{
-    console.log("In the Middleware 1");
-    next();
+app.use('/add-product',function(req, res, next){
+    console.log("In middleware");
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Click</button></form>')
 });
 
-app.use((req, res, next)=>{
-    console.log("In the Middleware 2");
-    res.send('HI');
+app.use('/product',function(req, res, next){
+    console.log(req.body);
+    res.redirect('/');
 });
 
-const server = http.createServer(app);
-server.listen(3000, function(){
-    console.log("Server started at port 30001");
+app.use('/',function(req, res, next){
+    console.log("Base Page");
+    res.send('Mutton Kushkava');
+});
+
+
+
+
+// const server = http.createServer(app);
+// server.listen(3000, function(){
+//     console.log("Server started at port 30001");
+// });
+
+app.listen(3000,function(){
+    console.log("Server started at port 3000");
 });
