9620d6b29bf424eb9e155718fde132d1f40709ae
AnthonyEdwardStark3000 <catchmesureshbabu8@gmail.com>
2022-12-27 21:40:17 +0530

    Just surfed status codes

--- Node JS/Node_JS_Restart/Shopping/error-check.js
+++ Node JS/Node_JS_Restart/Shopping/error-check.js
@@ -1,11 +1,11 @@
-const sum = function(a,b){
-    if(a&&b){
-        return a+b;
-    }
-    throw new Error('Need 2 numbers to perform arithmetic operation !');
-}
-try{console.log(sum(1));}
-catch(error){
-    console.log('error occurred!');
-    console.log(error);
-}
+// const sum = function(a,b){
+//     if(a&&b){
+//         return a+b;
+//     }
+//     throw new Error('Need 2 numbers to perform arithmetic operation !');
+// }
+// try{console.log(sum(1));}
+// catch(error){
+//     console.log('error occurred!');
+//     console.log(error);
+// }
