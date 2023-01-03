bab9894f9ebdfb4af7ec45eeace0d612f3b79fde
AnthonyEdwardStark3000 <catchmesureshbabu8@gmail.com>
2023-01-03 11:30:34 +0530

    Great intro to REST API'S

---
+++ Node JS/Node_JS_Restart/Shopping/REST_API_DEMO/routes/feed.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const feedController = require('../controllers/feed');
+const router = express.Router();
+
+//routes
+router.get('/posts',feedController.getPosts); //get-> /feed/posts
+router.post('/post',feedController.createPosts); //get-> /feed/posts
+module.exports = router;
